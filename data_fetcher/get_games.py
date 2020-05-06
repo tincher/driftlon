05a1a2ca02d845bd743f06fa1d7569cefb1e2a03
Joel Ewig <joel.ewig@gmail.com>
2020-05-06 23:02:14 +0200

    fetch_players & refactoring

--- data_fetcher/get_games.py
+++ data_fetcher/get_games.py
@@ -7,61 +7,62 @@ api_key = '?api_key=RGAPI-a35c93a9-3dc3-42da-8846-3149295755d7'
 api_url = 'https://{server}.api.riotgames.com{url_path}'
 
 
+# todo raise err if ratelimit hits
 def get_json_from_url(url):
-	return json.loads(requests.get(url).text)
+    return json.loads(requests.get(url).text)
 
 
 def get_account_id_for_name(name, server):
-	summoner_url = '/lol/summoner/v4/summoners/by-name/{}'.format(name) + api_key
-	complete_url = api_url.format(url_path=summoner_url, server=server)
-	return get_json_from_url(complete_url)['accountId']
+    summoner_url = '/lol/summoner/v4/summoners/by-name/{}'.format(name) + api_key
+    complete_url = api_url.format(url_path=summoner_url, server=server)
+    return get_json_from_url(complete_url)['accountId']
 
 
 def get_match_list_batch(account_id, server, begin_index, timestamp=0):
-	timestamp_url = ''
-	if timestamp > 0:
-		timestamp_url = '&beginTime=' + str(timestamp)
-	matchlist_url = '/lol/match/v4/matchlists/by-account/{}'.format(account_id) + api_key + timestamp_url
-	complete_url = api_url.format(url_path=matchlist_url, server=server) + '&beginIndex={}'.format(begin_index)
-	return get_json_from_url(complete_url)
+    timestamp_url = ''
+    if timestamp > 0:
+        timestamp_url = '&beginTime=' + str(timestamp)
+    matchlist_url = '/lol/match/v4/matchlists/by-account/{}'.format(account_id) + api_key + timestamp_url
+    complete_url = api_url.format(url_path=matchlist_url, server=server) + '&beginIndex={}'.format(begin_index)
+    return get_json_from_url(complete_url)
 
 
 def get_full_match_list_for_account(account_id, server, timestamp=0):
-	result, start_index = [], 0
-	response = get_match_list_batch(account_id, server, 0, timestamp)
-	result.extend(response['matches'])
-	while response['endIndex'] < response['totalGames']:
-		start_index = response['endIndex']
-		response = get_match_list_batch(account_id, server, start_index, timestamp)
-		result.extend(response['matches'])
-	return result
+    result, start_index = [], 0
+    response = get_match_list_batch(account_id, server, 0, timestamp)
+    result.extend(response['matches'])
+    while response['endIndex'] < response['totalGames']:
+        start_index = response['endIndex']
+        response = get_match_list_batch(account_id, server, start_index, timestamp)
+        result.extend(response['matches'])
+    return result
 
 
 def get_match_for_match_id(match_id, server):
-	match_url = '/lol/match/v4/matches/{}'.format(match_id) + api_key
-	complete_url = api_url.format(url_path=match_url, server=server)
-	return get_json_from_url(complete_url)
+    match_url = '/lol/match/v4/matches/{}'.format(match_id) + api_key
+    complete_url = api_url.format(url_path=match_url, server=server)
+    return get_json_from_url(complete_url)
 
 
 def get_number_of_patches(patch_count):
-	patches = json.loads(open('json_files/patches.json').read())
-	now = datetime.now()
-	for i in range(patch_count, len(patches)):
-		if datetime.strptime(patches[i]['date'], "%d. %B %Y") > now:
-			return patches[i - patch_count]
-	return patches[-1]
+    patches = json.loads(open('json_files/patches.json').read())
+    now = datetime.now()
+    for i in range(patch_count, len(patches)):
+        if datetime.strptime(patches[i]['date'], "%d. %B %Y") > now:
+            return patches[i - patch_count]
+    return patches[-1]
 
 
 def get_timestamp_for_last_number_of_patches(number_of_patches):
-	raw_date = get_number_of_patches(number_of_patches)['date']
-	patch_as_datetime = datetime.strptime(raw_date, "%d. %B %Y") + timedelta(hours=6)
-	return patch_as_datetime.timestamp()
+    raw_date = get_number_of_patches(number_of_patches)['date']
+    patch_as_datetime = datetime.strptime(raw_date, "%d. %B %Y") + timedelta(hours=6)
+    return patch_as_datetime.timestamp()
 
 
 def get_matchlist_for_player_since_number_of_patches(name, server, patch_count):
-	timestamp = int(get_timestamp_for_last_number_of_patches(patch_count))
-	account_id = get_account_id_for_name(name, server)
-	return get_full_match_list_for_account(account_id, server, timestamp)
+    timestamp = int(get_timestamp_for_last_number_of_patches(patch_count))
+    account_id = get_account_id_for_name(name, server)
+    return get_full_match_list_for_account(account_id, server, timestamp)
 
 
 # subdomain = 'euw1'
