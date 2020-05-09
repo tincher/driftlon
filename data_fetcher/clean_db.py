77cba3d7eb57023de6ad3e7dc1a0f4cc9a5e4435
Joel Ewig <joel.ewig@gmail.com>
2020-05-09 22:43:14 +0200

    clean_db added

---
+++ data_fetcher/clean_db.py
@@ -0,0 +1,34 @@
+from write_to_db import get_connection_for_collection_name
+import pymongo
+
+
+def get_duplicates_from_collection(collection):
+    pipeline = [{'$unwind': '$data'},
+                {"$group": {"_id": {"gameId": "$data.gameId", "platformId": "$data.platformId"}, "count": {"$sum": 1}}},
+                {"$match": {"_id": {"$ne": None}, "count": {"$gt": 1}}}]
+    duplicates = collection.aggregate(pipeline)
+    return duplicates
+
+
+def clean_collection(collection_name):
+    db, collection = get_connection_for_collection_name(collection_name)
+    duplicates = get_duplicates_from_collection(collection)
+    for duplicate in duplicates:
+        query = {'data.gameId': duplicate["_id"]['gameId'], 'data.platformId': duplicate["_id"]['platformId']}
+        for i in range(1, duplicate['count']):
+            result = collection.delete_one(query)
+            # print(query)
+            # result = collection.find_one(query)
+            # print(result)
+    db.close()
+
+
+def clean_matches_from_duplicates():
+    clean_collection('matches')
+
+
+def clean_players_from_duplicates():
+    clean_collection('player')
+
+
+clean_matches_from_duplicates()
