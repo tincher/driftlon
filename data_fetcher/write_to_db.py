48e6a6f916d34c5dfbbfc29925df09177c1e554a
Joel Ewig <joel.ewig@gmail.com>
2020-05-14 22:02:06 +0200

    requests wait wrt rate limit

--- data_fetcher/write_to_db.py
+++ data_fetcher/write_to_db.py
@@ -10,6 +10,48 @@ class DBWriter:
     def __init__(self):
         self.db_player, self.player_collection = get_connection_for_collection_name('player')
         self.db_matches, self.matches_collection = get_connection_for_collection_name('matches')
+
+    # def __del__(self):
+    #     db_player.close()
+    #     db_matches.close()
+
+    @staticmethod
+    def get_hash(value):
+        return hashlib.sha256(value.encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def insert_or_update(collection, query, content):
+        if len([x for x in collection.find(query)]) > 0:
+            collection.update_one(query, {'$set': content})
+        else:
+            collection.insert_one(content)
+
+    def update_user_timestamp(self, player):
+        query = {'id': self.get_hash(player['name'])}
+        new_timestamp = {"$set": {"timestamp": datetime.utcnow()}}
+        answer = player_collection.update_one(query, new_timestamp)
+
+    def write_user(self, player):
+        name, soloq_ids, pro_games = player['name'], player['soloq_ids'], player['pro_games']
+        hash_name = self.get_hash(name)
+        query = {'id': hash_name}
+        data = {'id': hash_name, 'name': name, 'soloq_ids': soloq_ids, 'pro_games': pro_games,
+                'timestamp': datetime.utcnow()}
+        self.insert_or_update(self.player_collection, query, data)
+
+    def write_game(self, game, player):
+        data = {'data': game, 'timestamp': datetime.utcnow(), 'player_id': self.get_hash(
+            player['name']), 'pro_games_count': int(player['pro_games'])}
+        query = {'game_id': game['gameId']}
+        self.insert_or_update(self.matches_collection, query, data)
+
+class DBWriter:
+    player_collection, matches_collection = None, None
+    db_player, db_matches = None, None
+
+    def __init__(self):
+        self.db_player, self.player_collection = get_connection_for_collection_name('player')
+        self.db_matches, self.matches_collection = get_connection_for_collection_name('matches')
         self.db_processed, self.processed_collection = get_connection_for_collection_name('processed_matches')
 
     @staticmethod
--- preprocessor/get_filtered_match.py
+++ preprocessor/get_filtered_match.py
@@ -13,35 +13,13 @@ import statistics
 import numpy as np
 import tensorflow as tf
 
-DBReader = DBReader()
-DBWriter = DBWriter()
-
 
 def get_random_matches_batch(batch_size):
-	db, collection = get_connection_for_collection_name('matches')
-	matches = list(collection.find({}))
-	random.shuffle(matches)
-	db.close()
-	return matches[:batch_size]
-
-
-def get_particpant_id(match):
-	participant_id = 0
-	player = DBReader.get_player_for_id(match['player_id'])
-	player_account_ids = [x['account_id'] for x in player['soloq_ids']]
-	for participant in match['data']['participantIdentities']:
-		if participant['player']['accountId'] in player_account_ids:
-			participant_id = participant['participantId']
-	return participant_id
-
-
-def get_processed_stats(stats, items_to_be_kept):
-	result = {}
-	for key in stats:
-		if key in items_to_be_kept:
-			result[key] = stats[key]
-	return result
-
+    db, collection = get_connection_for_collection_name('matches')
+    matches = list(collection.find({}))
+    random.shuffle(matches)
+    db.close()
+    return matches[:batch_size]
 
 def get_processed_timeline_data(match, items_to_be_kept):
 	offset = 2
