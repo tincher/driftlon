7ce4d1457336b0273aa41d4678b3b5b40de4018f
Joel Ewig <joel.ewig@gmail.com>
2020-05-22 11:07:32 +0200

    bucketize moved to preprocessor

--- neural_network/get_data.py
+++ neural_network/get_data.py
@@ -10,8 +10,6 @@ import numpy as np
 from driftlon_utils import *
 import tensorflow as tf
 
-
-
 def get_processed_matches_batch(batch_size=1):
     db, collection = get_connection_for_collection_name('processed_matches')
     matches = list(collection.find({}))
@@ -19,7 +17,7 @@ def get_processed_matches_batch(batch_size=1):
     db.close()
     return matches[:batch_size]
 
-def get_data_batch(batch_size):
+def get_batch(batch_size):
     match_batch = get_processed_matches_batch(batch_size)
     data, target = [], []
     for element in match_batch:
@@ -27,21 +25,7 @@ def get_data_batch(batch_size):
         target.append(element['target'])
     return data, target
 
-def get_bucketized_match(match):
-    bucket_list = [0, 4, 27, 28]
-    for entry in bucket_list:
-        print(match)
-        print(entry)
-        value = tf.strings.to_hash_bucket_strong(list(str(match[entry])), 20, [0, 0]).numpy()
-        match[entry] = value[0]
-    return match
 
 if __name__ == '__main__':
     batch_size = 142
-    data, target = get_data_batch(batch_size)
-    result, lengths = [], []
-    for entry in data:
-        bucket_match = get_bucketized_match(entry)
-        result.append(bucket_match)
-        lengths.append(len(bucket_match))
-    print(lengths)
+    data, target = get_batch(batch_size)
