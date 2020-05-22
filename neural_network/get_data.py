80a0cd8b328bf8362b57ac9e899eb4b702136e22
Joel Ewig <joel.ewig@gmail.com>
2020-05-22 21:37:22 +0200

    get_data

--- neural_network/get_data.py
+++ neural_network/get_data.py
@@ -4,28 +4,44 @@ sys.path.append(".")
 sys.path.append("/Users/joelewig/projects/driftlon")
 
 import pymongo
-import json
 import random
 import numpy as np
 from driftlon_utils import *
 import tensorflow as tf
+from datetime import datetime, timedelta
 
-def get_processed_matches_batch(batch_size=1):
+
+def get_player_ids_in_timespan(batch_size, timespan):
     db, collection = get_connection_for_collection_name('processed_matches')
-    matches = list(collection.find({}))
-    random.shuffle(matches)
+    timestamp_for_span = int((datetime.utcnow() - timespan).timestamp())
+    players = list(collection.find({'timestamp': {'$gt': timestamp_for_span}}).distinct('player_id'))
     db.close()
-    return matches[:batch_size]
+    return players[:batch_size]
 
-def get_batch(batch_size):
-    match_batch = get_processed_matches_batch(batch_size)
+def get_matches_batch(player_ids, batch_size=1, timespan=timedelta(weeks=2)):
+    db, collection = get_connection_for_collection_name('processed_matches')
+    timestamp_for_span = int((datetime.utcnow() - timespan).timestamp())
+    result = []
+    for player_id in player_ids:
+        db_result = collection.find({'timestamp': {'$gt': timestamp_for_span}, 'player_id': player_id})
+        result.append(list(db_result))
+    db.close()
+    return result[:batch_size]
+
+def get_batch(batch_size, timespan=timedelta(weeks=2)):
+    players = get_player_ids_in_timespan(batch_size, timespan)
+    player_ids = get_player_ids_in_timespan(batch_size, timespan)
+    db_result = get_matches_batch(player_ids, batch_size, timespan)
     data, target = [], []
-    for element in match_batch:
-        data.append(np.array(element['data']))
-        target.append(element['target'])
+    for i in range(len(db_result)):
+        data.append([np.array(match['data']) for match in db_result[i]])
+        target.append(db_result[i][0]['target'])
     return data, target
 
 
 if __name__ == '__main__':
-    batch_size = 142
-    data, target = get_batch(batch_size)
+    batch_size = 10
+    timespan = timedelta(weeks=2)
+    data, target = get_batch(batch_size, timespan)
+    print(data)
+    print(target)
