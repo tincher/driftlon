ea1dcd745cb2d03f6ab4d309828eb568a9ff8a8b
Joel Ewig <joel.ewig@gmail.com>
2020-05-23 20:40:42 +0200

    refactored data sources to files

---
+++ data_fetcher/api_layers/trackingthepros_layer.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+
+
+class TTPLayer:
+    def __init__(self):
+        self.subdomains = {'[NA]': 'na1', '[EU]': 'euw1', '[EUW]': 'euw1',
+                           '[KR]': 'kr', '[EUNE]': 'eun1', '[BR]': 'br1'}
+
+    # todo refactor
+    def get_soloq_ids_from_trackingthepros(self, name):
+        result = []
+        r = requests.get('https://www.trackingthepros.com/player/{}'.format(name))
+        if 'players' in r.url:
+            return None
+        soup = BeautifulSoup(r.text, features="lxml")
+        inner_info = soup.find_all("div", class_="player-info-inner")
+        card = [x for x in inner_info if x.find("h4", text='Accounts') is not None][0]
+        table = card.find("table")
+        element = table.find("tr")
+        while element is not None and not (element.has_attr('id') and 'inactive_link' in element['id']):
+            if element.has_attr('class') and 'inactive_account' in element['class']:
+                element = element.next_sibling
+                continue
+            td = element.find("td")
+            server = self.get_subdomain_for_region(td.find("b").getText().strip())
+            account_name = td.getText().split("]")[-1].strip()
+            result.append({'server': server, 'account_name': account_name})
+            element = element.next_sibling
+        return result
+
+    def get_subdomain_for_region(self, region):
+        return self.subdomains[region]
--- data_fetcher/fetch_pro_players_games.py
+++ data_fetcher/fetch_pro_players_games.py
@@ -3,10 +3,10 @@ sys.path.append(".")
 sys.path.append("..")
 
 from datetime import datetime
-from get_games import *
 from get_from_db import *
 from write_to_db import DBWriter
-from get_eligible_players import *
+from leaguepedia_layer import LPLayer
+from trackingthepros_layer import TTPLayer
 
 
 batch_size = 20
@@ -14,25 +14,27 @@ patch_count = 2
 top_leagues = ["LoL European Championship", "League Championship Series", "LoL Champions Korea", "LoL Pro League"]
 DBWriter = DBWriter()
 DBReader = DBReader()
-
+LPLayer = LPLayer()
+TTPLayer = TTPLayer()
+RiotLayer = RiotLayer()
 
 def fetch_all_users():
-    players = get_all_eligible_players()
+    players = LPLayer.get_all_eligible_players()
     for player in players:
-        player['soloq_ids'] = get_soloq_ids_from_trackingthepros(player['name'])
+        player['soloq_ids'] = TTPLayer.get_soloq_ids_from_trackingthepros(player['name'])
         if player['soloq_ids'] is not None:
             for soloq_id in player['soloq_ids']:
-                soloq_id['account_id'] = get_account_id_for_name(soloq_id['account_name'], soloq_id['server'])
+                soloq_id['account_id'] = RiotLayer.get_account_id_for_name(soloq_id['account_name'], soloq_id['server'])
         DBWriter.write_user(player)
 
 
 def fetch_user_batch(batch_size):
-    player_batch = get_random_batch_of_players(batch_size)
+    player_batch = LPLayer.get_random_batch_of_players(batch_size)
     for player in player_batch:
-        player['soloq_ids'] = get_soloq_ids_from_trackingthepros(player['name'])
+        player['soloq_ids'] = TTPLayer.get_soloq_ids_from_trackingthepros(player['name'])
         if player['soloq_ids'] is not None:
             for soloq_id in player['soloq_ids']:
-                soloq_id['account_id'] = get_account_id_for_name(soloq_id['account_name'], soloq_id['server'])
+                soloq_id['account_id'] = RiotLayer.get_account_id_for_name(soloq_id['account_name'], soloq_id['server'])
         DBWriter.write_user(player)
 
 
@@ -40,9 +42,9 @@ def fetch_games_for_oldest_batch(batch_size):
     players = DBReader.get_oldest_updated_batch_of_players(batch_size)
     for player in players:
         for soloq_id in player['soloq_ids']:
-            match_list = get_matchlist_for_player_since_number_of_patches(soloq_id['account_id'], soloq_id['server'], 1)
+            match_list = RiotLayer.get_matchlist_for_player_since_number_of_patches(soloq_id['account_id'], soloq_id['server'], 1)
             for match in match_list:
-                result = get_match_for_match_id(match['gameId'], soloq_id['server'])
+                result = RiotLayer.get_match_for_match_id(match['gameId'], soloq_id['server'])
                 DBWriter.write_game(result, player)
         # DBWriter.update_user_timestamp(player)
 
