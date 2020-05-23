4f247420f2b92555f6d1bebe1ac3913af45e4a33
Joel Ewig <joel.ewig@gmail.com>
2020-05-23 23:23:54 +0200

    initial nn; gitignore changed

--- neural_network/train_network.py
+++ neural_network/train_network.py
@@ -2,17 +2,18 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 
+from datetime import datetime
 
-mnist = tf.keras.datasets.mnist
-
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-print(x_train)
 
 # https://www.tensorflow.org/guide/keras/rnn#create_a_model_instance_and_compile_it
+# https://www.tensorflow.org/guide/keras/rnn
+
+(train_images, train_labels), _ = tf.keras.datasets.fashion_mnist.load_data()
+train_images = train_images / 255.0
+
 
 model = Sequential()
-model.add(LSTM(128, input_shape=(x_train.shape[1:]), activation='relu', return_sequences=True))
+model.add(LSTM(128, input_shape=(28, 28), activation='relu', return_sequences=True))
 model.add(Dropout(0.2))
 
 model.add(LSTM(128, activation='relu'))
@@ -22,3 +23,27 @@ model.add(Dense(32, activation='relu'))
 model.add(Dropout(0.2))
 
 model.add(Dense(10, activation='softmax'))
+
+
+model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+
+logdir="tb_logs/fit/" + datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=logdir)
+
+# Train the model.
+model.fit(
+    train_images,
+    train_labels,
+    batch_size=1000,
+    epochs=1,
+    callbacks=[tensorboard_callback])
+
+# model.fit(x_train, y_train,
+#           validation_data=(x_test, y_test),
+#           batch_size=batch_size,
+#           epochs=5)
+
+# model.summary()
