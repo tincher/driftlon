72272a9761a0d36a616a8d187ef9f98f14593b05
tincher <joel.ewig@gmail.com>
2020-11-01 12:16:42 +0100

    multiple bugfixes

--- data_fetcher/fetch.py
+++ data_fetcher/fetch.py
@@ -17,28 +17,23 @@ TTPLayer = TTPLayer()
 RiotLayer = RiotLayer()
 
 
-# TODO logging
-def fetch_all_pros():
+def fetch_pros(batch_size=0):
+    logging.info('FETCH: fetching pros - batch_size: {}'.format(batch_size))
     players = LPLayer.get_all_eligible_players()
+    if batch_size > 0:
+        players = players[:batch_size]
     for player in players:
         player['soloq_ids'] = TTPLayer.get_soloq_ids(player['name'])
         if player['soloq_ids'] is not None:
             for soloq_id in player['soloq_ids']:
                 soloq_id['account_id'] = RiotLayer.get_account_id_for_name(soloq_id['account_name'], soloq_id['server'])
-        DBWriter.write_user(player)
-
-
-def fetch_pro_batch(batch_size=20):
-    player_batch = LPLayer.get_random_batch_of_players(batch_size)
-    for player in player_batch:
-        player['soloq_ids'] = TTPLayer.get_soloq_ids(player['name'])
-        if player['soloq_ids'] is not None:
-            for soloq_id in player['soloq_ids']:
-                soloq_id['account_id'] = RiotLayer.get_account_id_for_name(soloq_id['account_name'], soloq_id['server'])
+                soloq_id['ranking'] = RiotLayer.get_rank_for_account_id(soloq_id['account_id']['id'], soloq_id['server'])
+                soloq_id['ranking']['last_checked'] = datetime.utcnow()
         DBWriter.write_user(player)
 
 
 def fetch_games_for_oldest_batch(batch_size=20):
+    logging.info('FETCH: fetching games for oldest - batch_size: {}'.format(batch_size))
     players = DBReader.get_oldest_updated_batch_of_players(batch_size)
     for player in players:
         for soloq_id in player['soloq_ids']:
@@ -51,19 +46,19 @@ def fetch_games_for_oldest_batch(batch_size=20):
         DBWriter.update_user_timestamp(player)
 
 def fetch_casuals(config_number):
+    logging.info('FETCH: fetching casuals - config: {}'.format(config_number))
     configs = [{'tier': 'challenger', 'division': ''}, {'tier': 'grandmaster', 'division': ''}, {'tier': 'master', 'division': ''}, {'tier': 'DIAMOND', 'division': 'I'}, {'tier': 'DIAMOND', 'division': 'II'}]
     queue ='RANKED_SOLO_5x5'
     subdomain = 'euw1'
-    player_batch = RiotLayer.get_players_from_division(queue, configs[config_number]['tier'], configs[config_number]['division'], subdomain)
-    for player in player_batch:
+    for player in RiotLayer.get_players_from_division(queue, configs[config_number]['tier'], configs[config_number]['division'], subdomain):
         DBWriter.write_user(player)
 
 
 def main(args):
-    logging.basicConfig(filename='driftlon.log',level=logging.INFO, format='%(asctime)s %(message)s')
+    logging.basicConfig(filename='/var/log/driftlon/driftlon.log',level=logging.DEBUG, format='%(asctime)s %(message)s')
     logging.info('start fetching - {}'.format(args))
     if args.type == 'pros':
-        fetch_pro_batch(args.batch_size)
+        fetch_pros(args.batch_size)
     elif args.type == 'casuals':
         fetch_casuals(args.config)
     elif args.type == 'games':
@@ -75,8 +70,8 @@ def main(args):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--type', type=str ,help='What do you want to fetch? (games, pros, casuals)')
-    parser.add_argument('--batch_size', type=int, default=10, help='What is the batch size?')
-    parser.add_argument('--config', type=int, default=1,  help='What config type for casuals? (1-5)')
+    parser.add_argument('--batch_size', type=int, default=0, help='What is the batch size?')
+    parser.add_argument('--config', type=int, default=1,  help='What config type for casuals? (0-4)')
     args = parser.parse_args()
     if main(args) == False:
         parser.print_help()
