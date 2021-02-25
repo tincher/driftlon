56368268257498b1150e173ac140723be57f6d6f
joel <joel.ewig@stud.uni-bamberg.de>
2021-02-25 21:55:15 +0100

    refactor folders, init dataset wrapper

---
+++ neural_network/dataset.py
@@ -0,0 +1,10 @@
+import pymongo
+from torch.utils.data import Dataset
+
+class DriftlonDataset(Dataset):
+    def __init__(self):
+        pass
+    def __len__(self):
+        pass
+    def __getitem__(self):
+        pass
