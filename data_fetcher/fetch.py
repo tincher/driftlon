b7484cdb49e79c44acc4dc8926398c50cae76445
joel <joel.ewig@stud.uni-bamberg.de>
2021-02-25 21:49:23 +0100

    requirements.txt init, datafetcher bug fixes

--- data_fetcher/fetch.py
+++ data_fetcher/fetch.py
@@ -6,6 +6,7 @@ from write_to_db import DBWriter
 from data_fetcher.api_layers.leaguepedia_layer import LPLayer
 from data_fetcher.api_layers.trackingthepros_layer import TTPLayer
 from data_fetcher.api_layers.riot_layer import RiotLayer
+from tqdm import tqdm
 
 batch_size = 20
 patch_count = 2
@@ -22,7 +23,7 @@ def fetch_pros(batch_size=0):
     players = LPLayer.get_all_eligible_players()
     if batch_size > 0:
         players = players[:batch_size]
-    for player in players:
+    for player in tqdm(players):
         player['soloq_ids'] = TTPLayer.get_soloq_ids(player['name'])
         if player['soloq_ids'] is not None:
             for soloq_id in player['soloq_ids']:
@@ -35,7 +36,7 @@ def fetch_pros(batch_size=0):
 def fetch_games_for_oldest_batch(batch_size=20):
     logging.info('FETCH: fetching games for oldest - batch_size: {}'.format(batch_size))
     players = DBReader.get_oldest_updated_batch_of_players(batch_size)
-    for player in players:
+    for player in tqdm(players):
         for soloq_id in player['soloq_ids']:
             if soloq_id['account_id'] is not None:
                 match_list = RiotLayer.get_matchlist_for_player_since_number_of_patches(soloq_id['account_id']['accountId'], soloq_id['server'], 1)
@@ -50,7 +51,7 @@ def fetch_casuals(config_number):
     configs = [{'tier': 'challenger', 'division': ''}, {'tier': 'grandmaster', 'division': ''}, {'tier': 'master', 'division': ''}, {'tier': 'DIAMOND', 'division': 'I'}, {'tier': 'DIAMOND', 'division': 'II'}]
     queue ='RANKED_SOLO_5x5'
     subdomain = 'euw1'
-    for player in RiotLayer.get_players_from_division(queue, configs[config_number]['tier'], configs[config_number]['division'], subdomain):
+    for player in tqdm(RiotLayer.get_players_from_division(queue, configs[config_number]['tier'], configs[config_number]['division'], subdomain)):
         DBWriter.write_user(player)
 
 
