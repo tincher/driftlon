8bb1e650f6950382637c72fd514997c1e8005ec7
joel <joel.ewig@gmail.com>
2021-08-10 18:16:30 +0200

    merge

---
+++ analysis/corr_matrix.py
@@ -0,0 +1,20 @@
+import os
+import pandas as pd
+import numpy as np
+import seaborn as sn
+
+# Loading the dataset
+BIKE = pd.read_csv("day.csv")
+
+# Numeric columns of the dataset
+numeric_col = ['temp','atemp','hum','windspeed']
+
+# Correlation Matrix formation
+corr_matrix = BIKE.loc[:,numeric_col].corr()
+<<<<<<< HEAD
+=======
+print(corr_matrix)
+>>>>>>> init
+
+#Using heatmap to visualize the correlation matrix
+sn.heatmap(corr_matrix, annot=True)
